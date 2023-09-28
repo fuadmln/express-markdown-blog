b34bbdf7f55faa7f83cabf85cd3fbaf61907f381
Fuad Maulana <fuadm03@gmail.com>
2023-09-28 14:54:15 +0700

    feat: set view engine to ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('start')
+  res.render('index')
 })
 
 app.listen(3000, () => console.log(`Server strated in port 3000`))
