64c6bc6cd61811a664c5af9c62405f1cccc5e6d6
Fuad Maulana <fuadm03@gmail.com>
2023-09-28 15:34:17 +0700

    feat: view all articles

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
+const articleRouter = require('./routes/articles')
+
 app.set('view engine', 'ejs')
+app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const articles = [
+    {
+      title: 'Blog Title',
+      createdAt: new Date(),
+      description: 'Blog descriptions'
+    },
+    {
+      title: 'Second Title',
+      createdAt: new Date(),
+      description: 'Another descriptions'
+    }
+  ]
+  res.render('index', { articles: articles })
 })
 
 app.listen(3000, () => console.log(`Server strated in port 3000`))
